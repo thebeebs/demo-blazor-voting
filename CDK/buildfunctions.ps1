828502d79db27d36cc319984dbc59ac07ea9c41b
Martin Beeby <martinbeeby@hotmail.com>
2020-12-01 14:42:20 +0000

    Adding CDK stuff

--- CDK/buildfunctions.ps1
+++ CDK/buildfunctions.ps1
@@ -9,16 +9,16 @@
     # Amazon.Lambda.Tools global tools package installed (dotnet tool install -g Amazon.Lambda.Tools).
     
     Write-Host "...Building Lambda function packages"
-    $project_path = Join-Path ".." ".."  ".." "BackEnd" "OnConnect"
+    $project_path = Join-Path ".." "BackEnd" "OnConnect"
     Write-Host $project_path
     $output_path = Join-Path "." "assets" "OnConnect.zip"
     Write-Host $output_path
     dotnet lambda package --project-location $project_path --output-package $output_path
-    $project_path = Join-Path ".." ".."  ".." "BackEnd" "OnDisconnect"
+    $project_path = Join-Path ".." "BackEnd" "OnDisconnect"
     $output_path = Join-Path "." "assets" "OnDisconnect.zip"  
     dotnet lambda package --project-location $project_path --output-package $output_path
-    $project_path = Join-Path ".." ".."  ".." "BackEnd" "OnSendMessage"
-    $output_path = Join-Path "." "assets" "OnSendMessage.zip"  
+    $project_path = Join-Path ".." "BackEnd" "SendMessage"
+    $output_path = Join-Path "." "assets" "SendMessage.zip"  
     dotnet lambda package --project-location $project_path --output-package $output_path
     
  
